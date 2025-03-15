c004b0a94d04c326a666f0d964410ea332a29855
hamid7856 <113382032+hamid7856@users.noreply.github.com>
2025-03-15 10:54:50 +0500

    Add files via upload

---
+++ video2/script.js
@@ -0,0 +1,59 @@
+// a = 5;
+// b = 2
+
+// console.log("Value of a =", a ,"& Value of b =", b)
+// console.log(a)
+// console.log("Pre increament of a is ", ++a)
+// a = 5;
+// console.log(a);
+// console.log("Post increament of a is", a++);
+// console.log(a);
+
+
+// --------------- CONDITIONS-----------------
+
+// age = 15;
+// if (age >= 18) { 
+//     console.log("You are eligble for passport");
+// }else{
+//     console.log("You are not eligible")
+// }
+
+// num = 90;
+// if (num %2==0) {
+//     console.log("Entered Value is even number");    
+// } else {
+//     console.log("Entered value is odd number")
+    
+// }
+
+//-----------Turnery Operator-----------
+//turnery operators take 3 inputs / pehle condition, phr agar true hai to output kya aega , then false hai to kya output hoga, basic trunery operarot syntax [age >=18 ? adult: Not Adult;]
+
+// let age = 16;
+// age >=18 ? console.log("adult"): console.log("Not Adult");
+
+//----------------------Practice exercise, check if the number is an output of 5 or not using prompt
+
+// let value = prompt("enter ant number")
+// console.log("value is ", value);
+// if (value %5 ===0) {
+//     console.log("Number is divisible by 5");
+// } else {
+//     console.log("Value is not divisible of 5");
+        
+// }
+
+//---------------code to give grades to students according to numbers
+
+// let TotalPer = 88;
+
+// if (TotalPer >= 80) {
+//     console.log("Your Grade is A+");
+// } else if (TotalPer >= 70) {
+//     console.log("Your Grade is A");
+// } else if (TotalPer >= 60) {
+//     console.log("Your Grade is B");
+// }else {
+//     console.log("You are below 60");
+// }
